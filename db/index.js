555467fa35403af1733d21a6d9bb367c23909639
Michael G Rios <michael.rios093@gmail.com>
2022-03-30 16:36:06 -0400

    started express

---
+++ db/index.js
@@ -0,0 +1,60 @@
+// so now we are doing the same thing in sequelize
+// bringing in the entire module/library
+// Sequelize needs pg just in case you were wondering 
+const Sequelize = require('sequelize');
+
+// faker is hving problems but casual is working a ok :)
+const casual = require('casual');
+
+// getting our string
+const { STRING } = Sequelize;
+// db most be a class, giving it our database adress (??) as well 
+const db = new Sequelize(process.env.DATABASE_URL || 'postgres://postgres:JerryPine@localhost/fordham_users');
+
+// this is how sequelize defines a table I assume I than use User.. should find out soon
+const User = db.define('User', {
+    email: {
+        type: STRING,
+        allowNull: false,
+        // Bamo like that sequelize gives you some useful functions
+        validate: {
+            isEmail: true
+        }
+    },
+    bio: {
+        type: STRING
+    }
+});
+
+//  so this is the famous hook and seems like it should be very useful in validating data and such
+User.beforeSave( user => {
+    // why does this not need user.dataValues.bio... not a clue..
+    // check to see if it has a bio if not add one it is cool
+    if(!user.bio){
+        user.bio = `${user.email} BIO is ${casual.text}`
+    };
+    // console.log(user.bio);
+});
+
+// now with sequelize
+const syncAndSeed = async()=> {
+    // I think this drops everything 
+    await db.sync({force: true});
+    // so a little less writing
+    await User.create({ email: 'moe@gmail.com', bio: "I am a bio"});
+    await User.create({ email: 'lucy@yahoo.com'});
+    await User.create({ email: 'curly@aol.com'});
+};
+
+// you can use npm unistall package name ?? WOW!!
+
+// not entirely sure if I understand how module.exports work
+// I can use it but should probably look into it a bit more
+module.exports = {
+    db,
+    syncAndSeed,
+    // this is strictly a way for us to think about it I believe
+    models: {
+        User
+    }
+}
--- server.js
+++ server.js
@@ -1,50 +1,21 @@
-// so now we are doing the same thing in sequelize
-// bringing in the entire module/library
-// Sequelize needs pg just in case you were wondering 
-const Sequelize = require('sequelize');
+// so now instead of importing soimeone elses code we are importing our code 
+const {db, syncAndSeed, models: { User} } = require('./db');
+const express = require('express');
 
-// faker is hving problems but casual is working a ok :)
-const casual = require('casual');
+const app = express();
 
-// getting our string
-const { STRING } = Sequelize;
-// db most be a class, giving it our database adress (??) as well 
-const db = new Sequelize(process.env.DATABASE_URL || 'postgres://postgres:JerryPine@localhost/fordham_users');
-
-// this is how sequelize defines a table I assume I than use User.. should find out soon
-const User = db.define('User', {
-    email: {
-        type: STRING,
-        allowNull: false,
-        // Bamo like that sequelize gives you some useful functions
-        validate: {
-            isEmail: true
-        }
-    },
-    bio: {
-        type: STRING
+// so this is like an api
+app.get('/users', async(req, res, next)=> {
+    try {
+        const users = await User.findAll();
+        res.send(users);
     }
-});
+    catch(ex){
+        next(ex);
+    }
+})
 
-//  so this is the famous hook and seems like it should be very useful in validating data and such
-User.beforeSave( user => {
-    // why does this not need user.dataValues.bio... not a clue..
-    // check to see if it has a bio if not add one it is cool
-    if(!user.bio){
-        user.bio = `${user.email} BIO is ${casual.text}`
-    };
-    // console.log(user.bio);
-});
 
-// now with sequelize
-const syncAndSeed = async()=> {
-    // I think this drops everything 
-    await db.sync({force: true});
-    // so a little less writing
-    await User.create({ email: 'moe@gmail.com', bio: "I am a bio"});
-    await User.create({ email: 'lucy@yahoo.com'});
-    await User.create({ email: 'curly@aol.com'});
-};
 
 const init = async()=> {
     // try catch is always good here as things can go wrong
@@ -55,6 +26,8 @@ const init = async()=> {
         await syncAndSeed();
         // it gives you a bit more information
         // console.log(await User.findAll());
+        const port = process.env.PORT || 3000;
+        app.listen(port, ()=> console.log(`listening on port ${port}`));
     }
     catch(ex){
         console.log(ex)
@@ -64,48 +37,3 @@ const init = async()=> {
 
 init();
 
-// you can use npm unistall package name ?? WOW!!
-
-/*
-const { Client } = require('pg');
-const client = new Client (process.env.DATABASE_URL || 'postgress://postgres:JerryPine@localhost/fordham_users');
-
-// we creat the database
-// apperently the SERIAL makes a special sequence thing in Postgres should look into it
-const syncAndSeed = async()=> {
-    const SQL = `
-    DROP TABLE IF EXISTS users;
-    CREATE TABLE users(
-        id SERIAL PRIMARY KEY,
-        email VARCHAR(50) NOT NULL UNIQUE
-    );
-    `;
-    await client.query(SQL);
-
-}
-// we get the users
-const getUsers = async() => {
-    return (await client.query('SELECT * FROM users;')).rows;
-};
-
-// SO again that is the safer way of adding data and I really should take a look at it at one point
-// not sure what the RETURNING does need to check on that too
-const createUser = async({email}) => {
-    return (await client.query('INSERT INTO users (email) VALUES($1) RETURNING *', [email])).rows[0]
-}
-
-const init = async()=> {
-    try {
-        await client.connect();
-        await syncAndSeed();
-        // not entirely sure why we put it in a variable I mean we are just seeding the database
-        const user = await createUser( {email: 'moe@gmail.com'});
-        console.log(await getUsers());
-    }
-    catch(ex){
-        console.log(ex)
-    }
-};
-
-init();
-*/
