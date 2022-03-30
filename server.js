960a8c280fb0c15f9f6fca98ee0b3f75d4dd7875
michaelrios2015 <michael.rios093@gmail.com>
2022-03-29 21:46:29 -0400

    started building the database

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
- 
+const { Client } = require('pg');
+const client = new Client (process.env.DATABASE_URL || 'postgress://postgres:JerryPine@localhost/fordham_users');
+
+
+const syncAndSeed = async()=> {
+    const SQL = `
+    DROP TABLE IF EXISTS users
+    CREATE TABLE users(
+        id SERIAL PRIMARY KEY
+    );
+    `;
+    await client.query(SQL);
+
+}
+
+
+const init = async()=> {
+    try {
+        await client.connect();
+    }
+    catch(ex){
+        console.log(ex)
+    }
+};
+
+init();
